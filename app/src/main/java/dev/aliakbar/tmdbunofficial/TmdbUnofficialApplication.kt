a12bf10a76929a1e1ddf0f3a6bfdc6b5cc547faa
AliAkbar <aliakbarranjoodt@gmail.com>
2023-08-05 14:01:06 +0330

    application class added

---
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/TmdbUnofficialApplication.kt
@@ -0,0 +1,16 @@
+package dev.aliakbar.tmdbunofficial
+
+import android.app.Application
+import dev.aliakbar.tmdbunofficial.data.AppContainer
+import dev.aliakbar.tmdbunofficial.data.DefaultAppContainer
+
+class TmdbUnofficialApplication: Application()
+{
+    lateinit var container: AppContainer
+
+    override fun onCreate()
+    {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
