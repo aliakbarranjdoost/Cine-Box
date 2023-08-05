10103dc804b65abcdd3aa2688af4de6fd62d9cb4
AliAkbar <aliakbarranjoodt@gmail.com>
2023-08-05 13:30:31 +0330

    retrofit service added

--- app/src/main/java/dev/aliakbar/tmdbunofficial/data/AppContainer.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/data/AppContainer.kt
@@ -1,5 +1,12 @@
 package dev.aliakbar.tmdbunofficial.data
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dev.aliakbar.tmdbunofficial.network.TMDBApiService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
 interface AppContainer
 {
 
@@ -7,5 +14,25 @@ interface AppContainer
 
 class DefaultAppContainer: AppContainer
 {
+    private val baseUrl = "https://api.themoviedb.org/3/"
+    private val bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzkwYjcwMzRkOTFhODU0YmE5MmUxOTlkMWQ2MTk3MiIsInN1YiI6IjYzMGYxMTg0MTI0MjVjMDA5ZDdkMjAzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xPKQ-BTT_SZqBtJKyQ36VoDDpqCr_BAp-b_NjOOXvhc"
+
+    private val okhttp = OkHttpClient.Builder().addInterceptor()
+    {   chain ->
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer $bearerToken")
+            .build()
+        chain.proceed(newRequest)
+    }
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .client(okhttp.build())
+        .baseUrl(baseUrl)
+        .build()
+
+    val retrofitService: TMDBApiService by lazy {
+        retrofit.create(TMDBApiService::class.java)
+    }
 
 }
