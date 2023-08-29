7732206c1f5756881ddaa83d758d9cf649790f68
AliAkbar <aliakbarranjdoost@gmail.com>
2023-08-29 08:12:34 +0330

    details screen & view model added

--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/details/DetailsScreen.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/details/DetailsScreen.kt
@@ -1,10 +1,98 @@
 package dev.aliakbar.tmdbunofficial.ui.details
 
+import android.telecom.Call.Details
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import dev.aliakbar.tmdbunofficial.R
+import dev.aliakbar.tmdbunofficial.data.Movie
+import dev.aliakbar.tmdbunofficial.ui.home.HomeViewModel
 
+private const val TAG: String = "DetailsScreen"
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(id: Int = 0)
+fun DetailsScreen(id: Int = 0,
+                  viewModel: DetailsViewModel = viewModel(factory = DetailsViewModel.factory)
+)
 {
-    Text(text = "Movie Details $id")
+    viewModel.getMovieDetails(id = id)
+    Log.d(TAG, "DetailsScreen: viewModel.getMovieDetails(id = $id)")
+    val uiState = viewModel.detailsUiState
+
+    when(uiState)
+    {
+        is DetailsUiState.Loading -> Text(text = "Loading")
+        is DetailsUiState.Success -> ShowMovieDetails(movie = uiState.movie)
+        is DetailsUiState.Error -> Text(text = "Error")
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowMovieDetails(movie: Movie)
+{
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = {
+                Text(
+                    "Centered TopAppBar",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                })
+        }
+    )
+    { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding))
+        {
+            AsyncImage(
+                model = painterResource(id = R.drawable.test),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+
+            Text(text = movie.title)
+        }
+    }
 }
--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/details/DetailsViewModel.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/details/DetailsViewModel.kt
@@ -1,16 +1,62 @@
 package dev.aliakbar.tmdbunofficial.ui.details
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import dev.aliakbar.tmdbunofficial.TmdbUnofficialApplication
 import dev.aliakbar.tmdbunofficial.data.DetailsRepository
+import dev.aliakbar.tmdbunofficial.data.Movie
+import dev.aliakbar.tmdbunofficial.data.Trend
+import dev.aliakbar.tmdbunofficial.data.toExternal
+import dev.aliakbar.tmdbunofficial.ui.home.HomeUiState
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface DetailsUiState
+{
+    data class Success(val movie: Movie) : DetailsUiState
+    data object Error : DetailsUiState
+    data object Loading : DetailsUiState
+}
 
 class DetailsViewModel (
     private val repository: DetailsRepository
 ): ViewModel()
 {
+    var detailsUiState: DetailsUiState by mutableStateOf(
+        DetailsUiState.Loading
+    )
+
+    init
+    {
+
+    }
+
+    fun getMovieDetails(id: Int)
+    {
+        viewModelScope.launch()
+        {
+            detailsUiState = try
+            {
+                DetailsUiState.Success(repository.getMovieDetails(id))
+            }
+            catch (e: IOException)
+            {
+                DetailsUiState.Error
+            }
+            catch (e: HttpException)
+            {
+                DetailsUiState.Error
+            }
+        }
+    }
+
     companion object
     {
         val factory: ViewModelProvider.Factory = viewModelFactory()
