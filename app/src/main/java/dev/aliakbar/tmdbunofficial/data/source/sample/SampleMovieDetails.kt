3cd05f3578dd59b970450f53841b1e1bc1c90988
AliAkbar <aliakbarranjdoost@gmail.com>
2023-09-05 10:18:23 +0330

    complete movie details sample model fo preview

--- app/src/main/java/dev/aliakbar/tmdbunofficial/data/source/sample/SampleMovieDetails.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/data/source/sample/SampleMovieDetails.kt
@@ -1,11 +1,15 @@
 package dev.aliakbar.tmdbunofficial.data.source.sample
 
+import dev.aliakbar.tmdbunofficial.data.Cast
 import dev.aliakbar.tmdbunofficial.data.Collection
 import dev.aliakbar.tmdbunofficial.data.Company
 import dev.aliakbar.tmdbunofficial.data.Country
+import dev.aliakbar.tmdbunofficial.data.Crew
 import dev.aliakbar.tmdbunofficial.data.Genre
+import dev.aliakbar.tmdbunofficial.data.Image
 import dev.aliakbar.tmdbunofficial.data.Language
 import dev.aliakbar.tmdbunofficial.data.Movie
+import dev.aliakbar.tmdbunofficial.data.Video
 
 val genres = listOf(
     Genre(id = 12, name = "Adventure"),
@@ -49,6 +53,90 @@ val languages = listOf(
     Language(iso = "en", englishName = "English", name = "English")
 )
 
+val cast = Cast(
+    id = 110,
+    adult = false,
+    gender = 2,
+    knownFor = "Acting",
+    name = "Viggo Mortensen",
+    originalName = "Viggo Mortensen",
+    popularity = 39.757F,
+    profilePath = "/vH5gVSpHAMhDaFWfh0Q7BG61O1y.jpg",
+    castId = 15,
+    character = "Aragorn",
+    creditId = "52fe421ac3a36847f8004591",
+    order = 0
+)
+
+val casts = mutableListOf<Cast>().apply { repeat(10) { this.add(cast) } }.toList()
+
+val crew = Crew(
+    id = 123,
+    adult = false,
+    gender = 2,
+    knownForDepartment = "Production",
+    name = "Barrie M. Osborne",
+    originalName = "Barrie M. Osborne",
+    popularity = 4.655F,
+    profilePath = "/xWtXYk6M5NFroddcQDviLlxOnkU.jpg",
+    creditId = "52fe421ac3a36847f800454f",
+    department = "Production",
+    job = "Producer"
+)
+
+val crews = mutableListOf<Crew>().apply { repeat(10) { this.add(crew) } }.toList()
+
+val video = Video(
+    iso6391 = "en",
+    iso31661 = "US",
+    name = "The Two Towers | The Lord of the Rings 4K Ultra HD | Warner Bros. Entertainment",
+    key = "nuTU5XcZTLA",
+    site = "YouTube",
+    size = 2160,
+    type = "Trailer",
+    official = true,
+    publishedAt = "2020-12-02T17:34:35.000Z",
+    id = "5fc8aafe3f8ede004000808c"
+)
+
+val videos = mutableListOf<Video>().apply { repeat(10) { this.add(video) } }.toList()
+
+val poster = Image(
+    iso6391 = "en",
+    aspectRatio = 0.667F,
+    height = 3000,
+    width = 2000,
+    filePath = "/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
+    voteAverage = 6.504F,
+    voteCount = 26
+)
+
+val posters = mutableListOf<Image>().apply { repeat(10) { this.add(poster) } }.toList()
+
+val backdrop = Image(
+    iso6391 = null,
+    aspectRatio = 1.778F,
+    height = 1080,
+    width = 1920,
+    filePath = "/tqj7NKj11keFuLzPsBDMUq2dOUO.jpg",
+    voteAverage = 5.388F,
+    voteCount = 4
+)
+
+val backdrops = mutableListOf<Image>().apply { repeat(10) { this.add(backdrop) } }.toList()
+
+val logo = Image(
+    iso6391 = "en",
+    aspectRatio = 4.751F,
+    height = 442,
+    width = 2100,
+    filePath = "/dMAXhf7jVsc8Qsx26wsoOmoQh3r.png",
+    voteAverage = 0F,
+    voteCount = 0
+)
+
+val logos = mutableListOf<Image>().apply { repeat(10) { this.add(logo) } }.toList()
+
 val movie = Movie(
     id = 120,
     title = "The Lord of the Rings: The Fellowship of the Ring",
@@ -77,10 +165,10 @@ val movie = Movie(
     collection = collection,
     productionCountries = countries,
     spokenLanguages = languages,
-    casts = emptyList(),
-    crews = emptyList(),
-    videos = emptyList(),
-    posters = emptyList(),
-    backdrops = emptyList(),
-    logos = emptyList(),
+    casts = casts,
+    crews = crews,
+    videos = videos,
+    posters = posters,
+    backdrops = backdrops,
+    logos = logos,
 )
