71aea7e51e9cd92a63e132d26e5a7d107e6ec844
AliAkbar <aliakbarranjdoost@gmail.com>
2023-10-05 11:22:17 +0330

    main part of showing trailer in home screen completed

--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/home/HomeScreen.kt
@@ -1,12 +1,14 @@
 @file:OptIn(
     ExperimentalFoundationApi::class, ExperimentalFoundationApi::class,
-    ExperimentalFoundationApi::class, ExperimentalFoundationApi::class
+    ExperimentalFoundationApi::class, ExperimentalFoundationApi::class,
+    ExperimentalFoundationApi::class
 )
 
 package dev.aliakbar.tmdbunofficial.ui.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -29,18 +32,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -283,25 +289,39 @@ fun SliderItem(trailer: Trailer)
                 .align(Alignment.TopStart)
         )
         {
-            /*if (isVideoFullScreen)
+            if (isVideoFullScreen)
             {
-                VideoDialog(videoId = trailer.first.id)
-                {
-                    isVideoFullScreen = false
-                }
-            }
-            else
-            {
-                YoutubeVideoPlayer(id = trailer.first.key, LocalLifecycleOwner.current)
-            }*/
+                val configuration = LocalConfiguration.current
 
-            Icon(
-                imageVector = Icons.Default.PlayCircleOutline,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(64.dp)
-                    .align(Alignment.Center)
-            )
+                when (configuration.orientation)
+                {
+                    Configuration.ORIENTATION_LANDSCAPE ->
+                    {
+                        VideoDialog(
+                            videoId = trailer.video.key,
+                            LocalLifecycleOwner.current,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                        {
+                            isVideoFullScreen = false
+                        }
+                    }
+                    else ->
+                    {
+                        VideoDialog(
+                            videoId = trailer.video.key,
+                            LocalLifecycleOwner.current,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(180.dp)
+                        )
+                        {
+                            isVideoFullScreen = false
+                        }
+                    }
+                }
+
+            }
 
             AsyncImage(
                 model = ImageRequest
@@ -314,8 +334,19 @@ fun SliderItem(trailer: Trailer)
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(230.dp)
+                    .clickable()
+                    {
+                        isVideoFullScreen = true
+                    }
             )
 
+            Icon(
+                imageVector = Icons.Default.PlayCircleOutline,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(64.dp)
+                    .align(Alignment.Center)
+            )
         }
 
         AsyncImage(
@@ -371,21 +402,24 @@ fun YoutubeVideoPlayer(id: String, lifecycleOwner: LifecycleOwner, modifier: Mod
                 }
             })
         }
-    })
+    }, modifier = Modifier.fillMaxSize())
 }
 
 @Composable
-fun VideoDialog(videoId: String, onDismissRequest: () -> Unit)
+fun VideoDialog(videoId: String,
+                lifecycleOwner: LifecycleOwner,
+                modifier: Modifier = Modifier,
+                onDismissRequest: () -> Unit)
 {
-    Dialog(onDismissRequest = { onDismissRequest() })
+    Dialog(
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+        onDismissRequest = { onDismissRequest() })
     {
         Card(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            shape = RoundedCornerShape(16.dp),
-        ) {
-            YoutubeVideoPlayer(id = videoId, lifecycleOwner = LocalLifecycleOwner.current)
+            modifier = modifier,
+        )
+        {
+            YoutubeVideoPlayer(id = videoId, lifecycleOwner = lifecycleOwner)
         }
     }
 }
@@ -397,5 +431,18 @@ fun PreviewSliderItem()
     TMDBUnofficialTheme()
     {
         Slider(trailers = trailers)
+    }
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun PreviewVideoDialog()
+{
+    TMDBUnofficialTheme()
+    {
+        VideoDialog(videoId = "", LocalLifecycleOwner.current)
+        {
+            
+        }
     }
 }
