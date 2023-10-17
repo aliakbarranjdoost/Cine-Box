a66930e9e0e02a0a1f2c36847d57d2e9b319ccf4
AliAkbar <aliakbarranjdoost@gmail.com>
2023-10-17 12:46:11 +0330

    top screen ui polish

--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/top/TopScreen.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/top/TopScreen.kt
@@ -2,7 +2,9 @@
 
 package dev.aliakbar.tmdbunofficial.ui.top
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,13 +13,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -32,16 +32,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
-import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
@@ -146,50 +148,59 @@ fun TopItem(top: Trend, onNavigateToDetails: () -> Unit, modifier: Modifier = Mo
         shape = CardDefaults.shape
     )
     {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-        )
+        Column(modifier = Modifier.fillMaxSize())
         {
-            AsyncImage(
-                model = ImageRequest
-                    .Builder(context = LocalContext.current)
-                    .data(top.poster)
-                    .build(),
-                placeholder = painterResource(id = R.drawable.backdrop_test),
-                contentDescription = top.title,
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier
-                    .weight(0.76F)
-                    .fillMaxWidth()
-            )
+            Box(modifier = Modifier.weight(0.76F).fillMaxWidth())
+            {
+                AsyncImage(
+                    model = ImageRequest
+                        .Builder(context = LocalContext.current)
+                        .data(top.poster)
+                        .build(),
+                    placeholder = painterResource(id = R.drawable.backdrop_test),
+                    contentDescription = top.title,
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
 
-            Text(
-                text = top.title,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier
-                    .weight(0.12F)
-                    .fillMaxWidth(),
-                style = MaterialTheme.typography.headlineSmall
-            )
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .align(Alignment.TopStart)
+                        .clip(CircleShape)
+                        .background(Color.Black),
+                    contentAlignment = Alignment.Center
+                )
+                {
+                    Text(
+                        text = top.rank.toString(),
+                        maxLines = 1,
+                        color = Color.White,
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier
+                            .padding(4.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
 
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .weight(0.12F)
+                    .weight(0.24F)
                     .fillMaxWidth()
             )
             {
                 Text(
-                    text = top.rank.toString(),
+                    text = top.title,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
                     modifier = Modifier
-                        .padding(4.dp)
-                        .weight(1f),
-                    style = MaterialTheme.typography.titleSmall
+                        .weight(1F)
+                        .fillMaxWidth(),
+                    style = MaterialTheme.typography.headlineSmall
                 )
 
                 IconButton(
