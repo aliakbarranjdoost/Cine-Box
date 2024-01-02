6a6cf11aa1e0173ff3ec4e09d79c195d4aa2218f
AliAkbar <aliakbarranjdoost@gmail.com>
2024-01-02 11:26:54 +0330

    bookmark icon color now turn yellow after bookmarking

--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/home/HomeScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
@@ -47,6 +48,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -592,6 +594,7 @@ fun Poster(
             Icon(
                 imageVector = if (isBookmark) Icons.Default.Bookmark else Icons.Default.BookmarkBorder,
                 contentDescription = null,
+                tint = if (isBookmark) Color.Yellow else LocalContentColor.current,
                 modifier = Modifier
                     .fillMaxSize()
             )
