364ff1e0b883ea65c442f43625d9835090d4314c
AliAkbar <aliakbarranjdoost@gmail.com>
2024-01-15 11:45:35 +0330

    person main model added

--- app/src/main/java/dev/aliakbar/tmdbunofficial/data/Person.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/data/Person.kt
@@ -7,3 +7,25 @@ data class Person(
     val gender: Int,
     val profileUrl: String
 )
+
+data class PersonDetails(
+    val adult: Boolean,
+    val alsoKnownAs: List<String>,
+    val biography: String,
+    val birthday: String,
+    val deathDay: String? = null,
+    val gender: Int,
+    val homepage: String? = null,
+    val id: Int,
+    val imdbId : String,
+    val knownForDepartment: String,
+    val name: String,
+    val placeOfBirth: String,
+    val popularity: Float,
+    val profileUrl: String,
+    val asMovieCast: PersonMovieAsCast,
+    val asTvCast: PersonTvAsCast,
+    val asMovieCrew: PersonAsMovieCrew,
+    val asTvCrew: PersonAsTvCrew,
+    val images: List<Image>
+)
