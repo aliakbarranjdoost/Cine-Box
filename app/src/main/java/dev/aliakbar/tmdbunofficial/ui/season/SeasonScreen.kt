9321a64b9dd7af7b9d75ec2f5f3b4dd6f9ed3d2f
AliAkbar <aliakbarranjdoost@gmail.com>
2024-01-26 12:48:22 +0330

    season screen ui polish

--- app/src/main/java/dev/aliakbar/tmdbunofficial/ui/season/SeasonScreen.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/ui/season/SeasonScreen.kt
@@ -1,23 +1,31 @@
 package dev.aliakbar.tmdbunofficial.ui.season
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,8 +36,6 @@ import dev.aliakbar.tmdbunofficial.data.source.sample.episodes
 import dev.aliakbar.tmdbunofficial.data.source.sample.seasonDetails
 import dev.aliakbar.tmdbunofficial.ui.components.CircularIndicator
 import dev.aliakbar.tmdbunofficial.ui.components.Image
-import dev.aliakbar.tmdbunofficial.ui.components.Rank
-import dev.aliakbar.tmdbunofficial.ui.components.ScoreBar
 import dev.aliakbar.tmdbunofficial.ui.components.TopBar
 import dev.aliakbar.tmdbunofficial.ui.theme.TMDBUnofficialTheme
 
@@ -40,9 +46,7 @@ fun SeasonScreen(
     viewModel: SeasonViewModel = viewModel(factory = SeasonViewModel.factory)
 )
 {
-    val uiState = viewModel.episodeListUiState
-
-    when (uiState)
+    when (val uiState = viewModel.episodeListUiState)
     {
         is SeasonUiState.Loading -> CircularIndicator()
         is SeasonUiState.Success ->
@@ -100,38 +104,67 @@ fun EpisodeList(
 @Composable
 fun EpisodeItem(episode: Episode, onNavigateToEpisode: (Int, Int) -> Unit)
 {
-    Card(onClick = { onNavigateToEpisode(episode.seasonNumber, episode.episodeNumber) }) {
-        Row(Modifier.height(100.dp)) {
+    val colorStops = listOf(
+        Color.Black.copy(alpha = 0.3f),
+        Color.Black.copy(alpha = 0.0f)
+    )
+
+    Card(
+        onClick = { onNavigateToEpisode(episode.seasonNumber, episode.episodeNumber) },
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+    ) {
+        Row() {
             Box(
-                modifier = Modifier
-                    .width(150.dp)
-                    .height(100.dp)
+                modifier = Modifier.size(width = 150.dp, height = 100.dp)
             )
             {
-                Image(url = episode.stillUrl!!, modifier = Modifier.fillMaxSize())
-
-                Rank(
-                    rank = episode.episodeNumber, modifier = Modifier
-                        .size(30.dp)
-                        .align(Alignment.TopStart)
-                        .padding(start = 2.dp, top = 2.dp)
+                Image(url = episode.stillUrl!!, modifier = Modifier.size(width = 150.dp, height = 100.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Brush.verticalGradient(colors = colorStops))
                 )
-
-                ScoreBar(
-                    score = episode.voteAverage,
-                    modifier =
-                    Modifier
-                        //.size(40.dp)
-                        .align(Alignment.BottomStart))
+                Text(
+                    text = episode.episodeNumber.toString(),
+                    maxLines = 1,
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.TopStart),
+                )
             }
 
-            Column(modifier = Modifier.padding(8.dp)) {
-                Text(text = episode.episodeNumber.toString() + ". " + episode.name)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = episode.overview, maxLines = 3)
+            Column {
+                Text(text = episode.name, modifier = Modifier.padding(8.dp))
+
+                Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+
+                Row(
+                    modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+                )
+                {
+                    Text(
+                        text = "%.${1}f".format(episode.voteAverage),
+                        fontWeight = FontWeight.Medium,
+                    )
+                    Text(
+                        text = "/10",
+                        fontWeight = FontWeight.Normal,
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = null,
+                        tint = Color.Yellow,
+                    )
+                }
             }
+
+
         }
-
     }
 }
 
