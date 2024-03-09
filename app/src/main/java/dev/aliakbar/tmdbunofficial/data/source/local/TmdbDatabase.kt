3940c41d3164f348a014f9d83ba6098e33802cf4
AliAkbar <aliakbarranjdoost@gmail.com>
2024-03-09 15:00:03 +0330

    room converter removed

--- app/src/main/java/dev/aliakbar/tmdbunofficial/data/source/local/TmdbDatabase.kt
+++ app/src/main/java/dev/aliakbar/tmdbunofficial/data/source/local/TmdbDatabase.kt
@@ -2,14 +2,11 @@ package dev.aliakbar.tmdbunofficial.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import dev.aliakbar.tmdbunofficial.util.Converters
 
 @Database(
     entities = [LocalBookmark::class],
     version = 8,
     exportSchema = false)
-@TypeConverters(Converters::class)
 abstract class TmdbDatabase: RoomDatabase()
 {
     abstract fun bookmarkDao(): LocalBookmarkDao
